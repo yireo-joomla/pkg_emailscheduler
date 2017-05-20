65aacafc0a3471fb2f60ae1284e4c44ef2c17167
Jisse Reitsma <info@yireo.com>
2017-05-20 12:51:45 +0200

    Fix issue of send-time not selectable

--- source/administrator/components/com_emailscheduler/fields/time.php
+++ source/administrator/components/com_emailscheduler/fields/time.php
@@ -2,10 +2,10 @@
 /**
  * Joomla! component Emailscheduler
  *
- * @author Yireo (info@yireo.com)
+ * @author    Yireo (info@yireo.com)
  * @copyright Copyright 2015
- * @license GNU Public License
- * @link http://www.yireo.com
+ * @license   GNU Public License
+ * @link      http://www.yireo.com
  */
 
 defined('JPATH_BASE') or die;
@@ -13,16 +13,30 @@ defined('JPATH_BASE') or die;
 /**
  * Form field that adds a time selector
  */
-class JFormFieldTime extends JFormField
+class JFormFieldTime extends JFormFieldText
 {
-    protected $type = 'Time';
+	/**
+	 * @var string
+	 */
+	protected $type = 'Time';
 
-    /**
-     * Method to get the field input markup.
-     *
-     * @return    string    The field input markup.
-     */
-    protected function getInput()
-    {
-    }
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return string $html   The field input markup.
+	 */
+	protected function getInput()
+	{
+		$html = parent::getInput();
+		$html = str_replace('class="inputbox', 'class="ui-timepicker-input ', $html);
+
+		$script   = "\njQuery(function() { jQuery('#" . $this->id . "').timepicker({timeFormat:'H:i:s'}); });";
+		$document = JFactory::getDocument();
+
+		$document->addStyleSheet(JURI::root() . 'media/com_emailscheduler/css/jquery.timepicker.css');
+		$document->addScript(JURI::root().'media/com_emailscheduler/js/jquery.timepicker.min.js');
+		$document->addScriptDeclaration($script);
+
+		return $html;
+	}
 }
